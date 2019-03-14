fa77247ef05046a305cac40b433dc9f3829e8846
admin <admin@itcast.cn>
2019-03-14 18:52:47 +0800

    tijiaogit

--- git_day01/src/com/itheima/git/FirstClass.java
+++ git_day01/src/com/itheima/git/FirstClass.java
@@ -3,6 +3,8 @@ package com.itheima.git;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("hehle");
+        System.out.println("111111");
+        System.out.println("22222222");
 
     }
 }
